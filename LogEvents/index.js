15a2c2225aae46171fac8608ca6ab34502520b6a
John Absolu <Johnson.absolu@hotmail.com>
2024-04-05 17:58:41 -0400

    Update

--- LogEvents/index.js
+++ LogEvents/index.js
@@ -2,3 +2,11 @@ const logEvents = require("./logEvents");
 const eventEmitter = require("events");
 
 class Emitter extends eventEmitter {};
+
+const myEmitter = new Emitter();
+
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  myEmitter.emit("Log", "Log event emitted!")
+})
