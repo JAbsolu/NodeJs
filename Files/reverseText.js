16f05a778d9259dcbcb2dd221d5846fde875a6e1
John Absolu <Johnson.absolu@hotmail.com>
2024-05-05 11:29:01 -0400

    Added new comments

--- Files/reverseText.js
+++ Files/reverseText.js
@@ -3,24 +3,28 @@ const path = require('node:path');
 const os = require('node:os');
 
 /**
- * @param {*} file // takes a file as argument
+ * @param {*} file // Takes a file as argument
  * @returns A clean readable reversed file
  */
 const reverseFile = (file) => {
-  let fileArray = file.split(' '); //convert the string into an array of strings
-  let reversed; // declare the reversed variable
-  /**
-   * Start iterating over the array of strings
-   */
+  // convert the string into an array of strings
+  let fileArray = file.split(' '); 
+
+  // declare the reversed variable
+  let reversed; 
+
+  // start iterating over the array of strings
   for (let i = 0; i < fileArray.length - 1; i+=1) {
+
     // call the reverseWord function and assin its return val to reversed
     reversed = reverseWord(fileArray[i]); 
-    fileArray[i] = reversed; // replace the item at i with the reversed word;
+
+    // replace the item at i with the reversed word;
+    fileArray[i] = reversed; 
   }
-  /**
-   * return a clean version of the file that ensures commas 
-   * are maintain where appririate and remove extra commas
-   */
+
+  // return a clean version of the file that ensures commas 
+  // are maintain where appririate and remove extra commas
   return fileArray.toString().replaceAll(',', ' ').replaceAll(',,', ',').replaceAll(' .', '. ');
 }
 
@@ -29,15 +33,21 @@ const reverseFile = (file) => {
  * @returns the word in reversed
  */
 const reverseWord = (word) => {
-  let reversed = ''; // initiate reversed as an empty string
-  let i = word.length -1; // initiate i
+  // initiate reversed as an empty string
+  let reversed = ''; 
+
+  // initiate i
+  let i = word.length -1; 
   
   // iterate over the chars of the word
   while (i != -1) {
-    reversed += word[i]; //add characters to reversed
+    // add characters to reversed
+    reversed += word[i]; 
+    // decrement i
     i-=1;
   }
-  return reversed; //return the reversed word
+  // return the reversed word
+  return reversed; 
 } 
 
 /**
@@ -46,10 +56,19 @@ const reverseWord = (word) => {
  * if the data exists print out the reversed text
  */
 fs.readFile(path.join(__dirname, 'fedexDataEngineer.txt'), 'utf-8', (err, data) => {
+  // if there is an error, print out an error
   if (err) console.log(err);
+
+  // if there is data
   if (data) {
+    
+    // call the reverseFile and pass the data, save res as reversedData
     const reversedData = reverseFile(data);
+
+    //Call the createNewFile function, pass in the file name and the data
     createNewFile("reversed", reversedData);
+        
+    //Call the createNewFile function, pass in the file name and the data
     createNewDesktopFile("reversed.txt", reversedData);
   }
 })
@@ -57,27 +76,33 @@ fs.readFile(path.join(__dirname, 'fedexDataEngineer.txt'), 'utf-8', (err, data) 
 /**
  * @param {*} newFileName takes in a new filename
  * @param {*} data takes in the data to be added to the new file
- * Creates a new file in the current directory
+ * creates a new file in the current directory
  */
 const createNewFile = (newFileName, data) => {
   // if the file doesn't have an extension, it defaults to a txt file
   if (newFileName.includes('.') !== true) newFileName = `${newFileName}.txt`;
-  //Create the file in the current dir
+
+  // create the file in the current dir
   fs.writeFile(`${__dirname}/${newFileName}`, data , (err) => {
-    if (err) console.log(err); // print out error, if there's an error
+
+    // print out error, if there's an error
+    if (err) console.log(err); 
   })
 }
 
 /**
  * @param {*} newFileName 
  * @param {*} data 
- * Creates a new file in desktop
+ * creates a new file in desktop
  */
 const createNewDesktopFile = (newFileName, data) => {
-    // if the file doesn't have an extension, it defaults to a txt file
+  // if the file doesn't have an extension, it defaults to a txt file
   if (newFileName.includes('.') !== true) newFileName = `${newFileName}.txt`;
-  //Create the file in desktop
+
+  // create the file in desktop
   fs.writeFile(`${os.homedir()}/Desktop/${newFileName}`, data , (err) => {
-    if (err) console.log(err); // print out error, if there's an error;
+
+    // print out error, if there's an error;
+    if (err) console.log(err); 
   })
 }
